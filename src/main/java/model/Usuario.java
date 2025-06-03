848ec0be61ef1eb0b136e24f8fc89c5f26760d3a
LeninPM <leninpruden09@gmail.com>
2025-06-02 22:48:01 -0500

    Segundo Avance Integrador

--- src/main/java/model/Usuario.java
+++ src/main/java/model/Usuario.java
@@ -1,13 +1,83 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author USUARIO
- */
 public class Usuario {
-    
+
+    private int idUsuario;
+    private String nombre;
+    private String apellido;
+    private String correoElectronico;
+    private String contrasena;
+    private String telefono;
+    private RolUsuario rol;  // ADMIN, USUARIO o TRABAJADOR o lo que haya , NOSE
+
+    public Usuario() {
+    }
+
+    public Usuario(int idUsuario, String nombre, String apellido, String correoElectronico, String contrasena, String telefono, RolUsuario rol) {
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correoElectronico = correoElectronico;
+        this.contrasena = contrasena;
+        this.telefono = telefono;
+        this.rol = rol;
+    }
+
+    // Getters y setters
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public RolUsuario getRol() {
+        return rol;
+    }
+
+    public void setRol(RolUsuario rol) {
+        this.rol = rol;
+    }
 }
